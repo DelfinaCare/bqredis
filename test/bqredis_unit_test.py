56ef6342a7732390fb991d5cef5ba110a17b5243
Ali Ebrahim <aebrahim@users.noreply.github.com>
2025-09-11 23:37:33 +0300

    Fix deadlock on thread exhaustion. (#10)
    We now use a second threadpool for accepting new requests.

--- test/bqredis_unit_test.py
+++ test/bqredis_unit_test.py
@@ -1,6 +1,7 @@
 import concurrent.futures
 import hashlib
 import io
+import threading
 import unittest.mock
 
 import fakeredis
@@ -144,6 +145,46 @@ class TestBQRedis(unittest.TestCase):
             self.assertEqual(execution_mock.call_count, 1)
             with self.assertRaises(MockException):
                 self.cache.query(self.query_str).result(timeout=1)
+            self.assertEqual(execution_mock.call_count, 2)
+
+    def test_executor_exhaustion(self):
+        self.executor.shutdown(wait=False)
+        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+        self.cache.executor = self.executor
+        key1 = self.cache.redis_key_prefix + self.query_hash
+        query_str2 = "SELECT baz from qux;"
+        key2 = (
+            self.cache.redis_key_prefix
+            + hashlib.sha256(query_str2.encode()).hexdigest()
+        )
+
+        expected_result1 = pa.RecordBatch.from_arrays(
+            [pa.array(["ZW", "ZM", "ZA"])], names=["alpha_2_code"]
+        )
+        expected_result2 = pa.RecordBatch.from_arrays(
+            [pa.array(["ZA", "ZM", "ZW"])], names=["alpha_2_code"]
+        )
+        block = threading.Lock()
+
+        def _mock_query_executor(query: str, key: str) -> bqredis._QueryResult:
+            nonlocal block
+            with block:
+                if key == key1:
+                    return _query_result(key1, expected_result1)
+                if key == key2:
+                    return _query_result(key2, expected_result2)
+                raise KeyError(key)
+
+        with self.mock_execute_query_to_bytes() as execution_mock:
+            block.acquire(timeout=1)
+            execution_mock.side_effect = _mock_query_executor
+            self.assertEqual(execution_mock.call_count, 0)
+            query1 = self.cache.query(self.query_str)
+            query2 = self.cache.query(query_str2)
+            # Allow the functions to proceed without enough threads in the threadpool
+            block.release()
+            self.assertEqual(query1.result(timeout=1), expected_result1)
+            self.assertEqual(query2.result(timeout=1), expected_result2)
             self.assertEqual(execution_mock.call_count, 2)
 
 
